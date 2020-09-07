d8bffe3c7f2e1d5326832d5c7b9a33b71e89d0a8
Maxime - Sylvestre <maxime.madrau@icloud.com>
2020-09-07 13:10:39 +0200

    Initial Commit

---
+++ swift-libimobiledevice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-libimobiledevice
+//
+//  Created by Maxime on 07/09/2020.
+//  Copyright © 2020 Madrau. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
